5669946793c5cc64e3bb16574564b1bb08960d3c
henryxie2093 <henryxie2093@gmail.com>
2018-04-12 19:22:25 +0800

    change some ui

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <transition>
-      <router-view class="child-view"></router-view>
+      <router-view></router-view>
     </transition>
   </div>
 </template>
@@ -10,29 +10,10 @@
 export default {
   name: "app",
   data() {
-    return {
-      transitionName: "slide-left"
-    };
+    return {};
   }
 };
 </script>
 
 <style lang="less" scoped>
-.child-view {
-  position: absolute;
-  width: 100vw;
-  transition: all 1s ease;
-}
-
-.slide-left-enter,
-.slide-right-leave-active {
-  opacity: 0;
-  transform: translate(100px, 0);
-}
-
-.slide-left-leave-active,
-.slide-right-enter {
-  opacity: 0;
-  transform: translate(0px, 0);
-}
 </style>
